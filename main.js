cddaf0102a6090609eeb992c47b55462159896e4
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-01-25 16:57:35 -0300

    Projeto Finalizado 🐱‍🏍

--- main.js
+++ main.js
@@ -18,6 +18,19 @@ function changeSocialMediaLinks() {
     }
 }
 
-function getGituubInfos
+function getGitHubInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`;
+
+    fetch(url)
+        .then(response =>  response.json())
+        .then(data => {
+            UserName.textContent = data.name;
+            UserBio.textContent = data.bio;
+            UserLink.href = data.html_url;
+            UserLogin.textContent =  `@${data.login}`;
+            UserImage.src = data.avatar_url;
+        });
+}
 
 changeSocialMediaLinks();
+getGitHubInfos();
